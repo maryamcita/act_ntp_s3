9514913d8d178401886b44013049ffb36906f1e5
Samuel <Samuelbernal0809@gmail.com>
2025-07-24 10:16:52 -0500

    cambio 3

---
+++ src/ejercicio_06.py
@@ -0,0 +1,8 @@
+numero_pricipal = 3
+multiplos = 0
+print("Los primeros 15 múltiplos de 3 son:")
+while multiplos < 15:
+    print(numero_pricipal)
+    numero_pricipal+= 3
+    multiplos += 1
+
---
+++ src/ejercicio_07.py
@@ -0,0 +1,6 @@
+palabra="Manzana"
+contar_letra=0
+for letra in palabra:
+    if letra =="a":
+        contar_letra+=1
+print (f"la plabra Manzana Tiene {contar_letra} a ")
